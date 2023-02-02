5b9c6bbefebbff2eca85f9de9daf2cd86326f8a9
mahdiarrashidi <64040502+mahdiarrashidi@users.noreply.github.com>
2023-02-02 21:42:13 +0000

    Create AutoBackup.bat

---
+++ winSCP/AutoBackup.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set MYUSERNAME=user
+
+set MYPASSWORD="A1 ohli ldr,gd?#{[]}$~`\"
+
+Rem #This line starts a WinSCP script file
+
+start C:\home\Rashidi\WinSCP\WinSCP.com /ini=nul /script=C:\home\Rashidi\WinSCP\BackupPlans\vw1\AutoBackup_vw1.script /log=C:\home\Rashidi\WinSCP\BackupPlans\vw1\AutoBackup_vw1.log
+
+Rem #This line delets the files older than 70 days
+
+ForFiles /p "D:\vw1" /s /d -70 /c "cmd /c del @file"
