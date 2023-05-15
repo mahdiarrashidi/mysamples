bcf48734c070b37a4302a913306b584b9dc74a39
mahdiarrashidi <64040502+mahdiarrashidi@users.noreply.github.com>
2023-05-15 13:12:06 +0100

    Update AutoBackup.bat

--- winSCP/AutoBackup.bat
+++ winSCP/AutoBackup.bat
@@ -10,4 +10,4 @@ start C:\home\Rashidi\WinSCP\WinSCP.com /ini=nul /script=C:\home\Mahdiar\WinSCP\
 
 Rem #This line delets the files older than 70 days
 
-ForFiles /p "D:\mybackups" /s /d -70 /c "cmd /c del @file"
+ForFiles /p "D:\mybackups" /s /d -70 /c "cmd /c del @file" 
