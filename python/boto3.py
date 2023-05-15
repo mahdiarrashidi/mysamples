9fe0988b22a36ddef861d66bb78e8c82d96d395e
mahdiarrashidi <64040502+mahdiarrashidi@users.noreply.github.com>
2023-05-15 12:31:59 +0100

    Update boto3.py

--- python/boto3.py
+++ python/boto3.py
@@ -1,25 +1,20 @@
-#boto3 Script To Retrieve EMR Master IP
-
-
 import boto3
-import json
 
-boto_client_emr = boto3.client("emr")
-cluster_id = "j-DFGDSFGREWG"
- 
-def get_emr_master_pvt_ip(boto_client_emr, cluster_id):
-    emr_list_instance_rep = boto_client_emr.list_instances(
-        ClusterId=cluster_id,
-        InstanceGroupTypes=[
-            'MASTER',
-        ],
-        InstanceStates=[
-            'RUNNING',
-        ]
-    )
-  
-    return emr_list_instance_rep["Instances"][0]["PrivateIpAddress"]
+# Create a connection to the EC2 service
+ec2_client = boto3.client('ec2', region_name='eu-west-2')
 
-emr_master_ip = get_emr_master_pvt_ip(boto_client_emr, cluster_id)
+# Define parameters for the EC2 instance
+instance_params = {
+    'ImageId': 'ami-09744628bed84e434',
+    'InstanceType': 't2.micro',
+    'MinCount': 1,
+    'MaxCount': 1
+}
 
-print("EMR master IP is" + " " + emr_master_ip)
+# Create the EC2 instance
+response = ec2_client.run_instances(**instance_params)
+
+# Get the Instance ID of the created EC2 instance
+instance_id = response['Instances'][0]['InstanceId']
+
+print("EC2 instance created with Instance ID:", instance_id)
